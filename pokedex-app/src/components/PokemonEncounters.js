1822f377f5498804f011e27304fb0c3ce63db6e5
arielmerin <arielmerino@ciencias.unam.mx>
2021-06-14 06:18:22 -0500

    FINISHED

--- pokedex-app/src/components/PokemonEncounters.js
+++ pokedex-app/src/components/PokemonEncounters.js
@@ -1,12 +1,101 @@
 import {Link,useParams} from 'react-router-dom'
+import {useEffect, useState} from "react";
+import getPokeByName from "../services/getPokeByName";
+import axios from "axios";
+import capitalize from "../services/Capitalize";
+import useColorThief from "use-color-thief";
+
+import styled from "styled-components";
+
+const EncountersPage = styled.div`
+width: 100vw;
+min-width: 100%;
+height: 100%;
+min-height: 100vh;
+background-color: ${props => props.colorB };
+margin: 0;
+`
+
+
+const Name = styled.h1`
+color: white;
+font-family: 'Hammersmith One', sans-serif;
+font-size: 2.5em;
+margin: 0;
+`
+
+const EncountersUl = styled.ul`
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+padding: 0;
+margin: 0 auto;
+width: 80vw;
+padding: 2em 0;
+background-color: white;
+border-radius: 1em;
+box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
+`
+const EncountersLi = styled.li`
+display: flex;
+width: 100%;
+justify-content: center;
+list-style: none;
+`
+
 
 const PokemonEncounters = () =>{
-	const {id} = useParams()
+	const [img, setImg] = useState('')
+	const [encountersURL, setEncountersURL] = useState('')
+	const [encounters, setEncounters] = useState([ ])
+	const [avColor, setAvColor]   = useState('whie')
 
-	return( <div>
-		<h1>{id}</h1>
-		<Link to={`pokemon/${id}`}>Go Back</Link>
-	</div>)
+	const {id} = useParams()
+	useEffect(()=>{
+		getPokeByName(id).then((res)=>{
+			setImg(res.data.sprites.other["official-artwork"].front_default)
+			setEncountersURL(res.data.location_area_encounters)
+		})
+	}, [id])	
+	
+	useEffect(()=>{
+		if(encountersURL){
+			axios.get(encountersURL).then((res)=>{
+				if(res.data.length > 0){
+					setEncounters(res.data)
+				}
+			})
+		}
+	}, [encountersURL])
+
+	const listEncounters = encounters.map((value, index)=>{
+		return <EncountersLi key={index}>
+			{capitalize(value.location_area.name.replaceAll('-', ' '))}
+		</EncountersLi>
+	})
+
+	const { color } = useColorThief(img, {
+		format: 'hex',
+		colorCount: 10,
+		quality: 10,
+	});
+
+	useEffect(() => {
+		setAvColor(color)
+	}, [color])
+	
+	return( <EncountersPage colorB={avColor}>
+		<Link to={`/pokemon/${id}`}>Go Back</Link>
+		<div style={{display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center', height: '90vh'}}>
+			<div>
+				<Name>{capitalize(id)}</Name>
+				<Name>Encounters</Name>
+			</div>
+			<EncountersUl>
+				{encounters.length > 0 ? listEncounters: <div>No encouters for show</div>}
+			</EncountersUl>
+		</div>
+	</EncountersPage>)
 }
 
 export default PokemonEncounters
--- pokedex-app/src/views/PokemonPage.js
+++ pokedex-app/src/views/PokemonPage.js
@@ -5,6 +5,7 @@ import getPokeByName from "../services/getPokeByName";
 import useColorThief from "use-color-thief";
 import capitalize from "../services/Capitalize";
 import breakpoint from "../breakpoints";
+import "../Animation.css"
 
 const NumberContainer = styled.div`
   	display: flex;
@@ -14,7 +15,7 @@ const NumberContainer = styled.div`
   	width: 12%;
   	margin: 2px 0;
   	padding: 2px;
-  	color: ${props => props.colorS || 'black'};
+  	color: ${props => `rgb(${props.colorS.join()})` || 'black'};
   	border-radius: 10rem;
   	background-color: white;
 	`
@@ -37,8 +38,9 @@ const StatTotal = styled.div`
 	`
 const Stat = styled.div`
 		width: ${props => `${props.porcentage}%` || '0%'};
-		background-color: ${props => props.colorP || 'white'};
+		background-color: ${props => `rgb(${props.colorP.join()})` || 'white'};
 		margin: 2px;
+		padding: 3px 0;
 		font-size: 0.7em;
 		@media only screen and ${breakpoint.device.sm}{
 		font-size: 1.5em;
@@ -64,7 +66,7 @@ const NameStyled = styled.h1`
 `
 
 const PokemonPageStyled = styled.div`
-		background: ${props => props.colorB || '#fff'};
+		background: ${props => `rgb(${props.colorP.join()})` || '#fff'};
 		height: 100%;
 		min-height: 100vh;
 		display: flex;
@@ -73,11 +75,13 @@ const PokemonPageStyled = styled.div`
 		grid-gap: 10px;
 		width: 100vw;
 		justify-content: center;
+}
 	`
 
 const PokemonImageContainer = styled.div`
+	margin-top: 2.5em;
 	display: flex;
-	width: 100%;
+	width: 90%;
 `
 const PokemonImage = styled.img`
 	display: flex;
@@ -94,9 +98,11 @@ const InfoContainer = styled.div`
 	min-width:250px;	
 `
 const NumerStyled = styled.h3`
+	display: flex;
+    justify-content: center;
 	margin: 0;
 	color: white;
-	width: 100vw;
+	width: ${props=> props.ancho || '100vw'};
 `
 const StatsArrayContainer = styled.div`
 	display: flex;
@@ -119,9 +125,10 @@ const ImgTypeContainer = styled.div`
 
 const TypeImagesContainer = styled.div`
 		display: flex;
+		flex-wrap: wrap;
 		width: 100%;
 		margin: 10px 0;
-		justify-content: space-around;
+		justify-content: ${props => props.justify || 'space-around'};
 `
 
 const SpanTypes = styled.span`
@@ -130,13 +137,75 @@ const SpanTypes = styled.span`
 	font-size: 2rem;
 `
 
+const AbilityItem = styled.div`
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+	flex-wrap: wrap;
+	& img{
+	width: 10%;
+	}
+`
+
+const MoveItem = styled.div`
+		background-color: ${props => `rgb(${props.colorFont.join()})` || 'white'};
+		padding: 4px 6px;
+		border-radius: 2rem;
+		margin-top: 6px;
+		display: flex;
+		color: white;
+		align-items: center;
+		box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+	& span{
+		background-color: white;
+		color:${props => `rgb(${props.colorFont.join()})` || 'white'};
+		margin: 0 3px;
+		width: 1rem;
+		padding: 2px;
+		font-weight: bold;
+		justify-content: center;
+		border-radius: 0.5rem;
+		font-size: 0.7rem;
+		display: flex;
+		align-items: center;
+	}
+`
+
+const HomeImgContainer = styled.div`
+	display: flex;
+	position: absolute;
+	top: 5px;
+	left: 0;
+	overflow: hidden;
+	justify-content: flex-start;
+`
+
+const HomeImg = styled.img`	
+	width: 3.5rem;
+`
+
+const EncountersButton = styled.button`
+	box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+	margin: 2em 0 5em 0;
+	border: none;
+	padding: 0.8em 1em;
+	border-radius: 5em;
+	font-family: 'Hammersmith One', sans-serif;
+	font-size: 1em;
+	color: ${props => `rgb(${props.colorFont.join()})` || 'black'};
+	`
+
 const PokemonPage = ( ) =>{
-	const [avColor, setAvColor] = useState('white')
+	const [avColor, setAvColor] = useState([0,0,0])
 	const [img, setImg] = useState('')
 	const [types, setTypes] = useState([])
 	const [name, setName] = useState('')
 	const [numPokedex, setNumPokedex] = useState(0)
 	const [statsArray, setStatsArray] = useState([])
+	const [abilities, setAbilities] = useState([])
+	const [moves, setMoves] = useState([])
+	const [weight, setWeight] = useState(0)
+	const [height, setHeight] = useState(0)
 	const {id} = useParams()
 
 	useEffect(()=>{
@@ -146,23 +215,28 @@ const PokemonPage = ( ) =>{
 			setNumPokedex(res.data.order)
 			setTypes(res.data.types.slice())
 			setStatsArray(res.data.stats)
+			setAbilities(res.data.abilities)
+			setMoves(res.data.moves)
+			setWeight(res.data.weight)
+			setHeight(res.data.height)
 		})
 	}, [id])
 
 	const source = img;
 
 	const { color, palette } = useColorThief(source, {
-		format: 'hex',
+		format: 'rgb',
 		colorCount: 10,
 		quality: 10,
 	});
+
+
 
 	useEffect(() => {
 		if(color){
 			setAvColor(color)
 		}
 	}, [palette, color]);
-
 
 	const listTypesImages = types.map((value, index) =>{
 		const name = capitalize(value.type.name)
@@ -176,6 +250,12 @@ const PokemonPage = ( ) =>{
 		return <SpanTypes key={index}>{name} </SpanTypes>
 	})
 
+	const listAbilities = abilities.map((value, index) =>{
+		return <AbilityItem key={index}>
+			<SpanTypes>{capitalize(value.ability.name)} </SpanTypes>
+			{value.is_hidden ? <img src='./openeye.png' alt={value.ability.name} />: <img src="/closedeye.png" alt={value.ability.name} />}
+		</AbilityItem>
+	})
 
 	const listStats = statsArray.map((vale, index)=>{
 		return <StatContainer key={index}>
@@ -188,9 +268,19 @@ const PokemonPage = ( ) =>{
 		</StatContainer>
 	})
 
+	const listMoves = moves.map((value, index)=>{
+		return <MoveItem key={index} colorFont={palette ? palette[index % 10] : avColor}>
+			{capitalize(value.move.name)} <span>{index +1}</span>
+		</MoveItem>
+	})
+
 	return(
-		<PokemonPageStyled colorB={avColor} >
-			<Link to='/pokedex'>home</Link>
+		<PokemonPageStyled colorP={avColor} >
+			<Link to='/pokedex'>
+				<HomeImgContainer>
+					<HomeImg className='home' src='./pokeball.png' alt='pokeball' />
+				</HomeImgContainer>
+			</Link>
 			< PokemonImageContainer>
 				<PokemonImage id={name} src={img} alt={name}/>
 			</PokemonImageContainer>
@@ -208,8 +298,27 @@ const PokemonPage = ( ) =>{
 				<StatsArrayContainer>
 					{statsArray && listStats}
 				</StatsArrayContainer>
+
+				<TypeImagesContainer>
+					{listAbilities}
+				</TypeImagesContainer>
+				<TypeImagesContainer>
+					<div style={{width: '40vw'}}>
+						<NumerStyled ancho='50vw'> Weihgt </NumerStyled>
+						<NumerStyled ancho='50vw'>{weight}</NumerStyled>
+					</div>
+					<div style={{width: '40vw'}}>
+						<NumerStyled ancho='40vw'>Height</NumerStyled>
+						<NumerStyled ancho='40vw'>{height}</NumerStyled>
+					</div>
+				</TypeImagesContainer>
+				<TypeImagesContainer justify='space-between'>
+					{listMoves}
+				</TypeImagesContainer>
+
+
 				<Link to={`/pokemon/${name}/encounters`}>
-					<button>Encounters</button>
+					<EncountersButton colorFont={avColor} >Encounters</EncountersButton>
 				</Link>
 			</InfoContainer>
 		</PokemonPageStyled>
